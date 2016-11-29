4fb6184f2904168a796389e7b0a288c685ba54f5
HokieGeek <andres.f.perez@gmail.com>
2016-11-29 17:08:55 +0000

    Here is an idea

---
+++ provision.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/HokieGeek/hokiegeek-services.git
+
+git --git-dir=./hokiegeek-services/.git remote set-url origin git@github.com:HokieGeek/hokiegeek-services.git
+
+sudo hokiegeek-services/deploy.sh
