81dce4697eb2c24b1da8d72f3bf2671d0e74cf2c
HokieGeek <andres.f.perez@gmail.com>
2016-12-12 16:17:29 -0500

    I have hope

--- provision.sh
+++ provision.sh
@@ -2,4 +2,5 @@ git clone https://github.com/HokieGeek/hokiegeek.net-cfg.git
 
 git --git-dir=./hokiegeek.net-cfg/.git remote set-url origin git@github.com:HokieGeek/hokiegeek-services.git
 
-sudo hokiegeek.net-cfg/services/deploy.sh
+# sudo hokiegeek.net-cfg/services/deploy.sh
+ansible-playbook -i ${1}, playbooks/services.yml
